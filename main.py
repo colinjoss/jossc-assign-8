b6ab3284bb88612a926adad8632000b79ac23bdd
Colin Joss <colindjoss@gmail.com>
2022-05-20 18:05:55 -0700

    deleted files, basic text test'

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from google.cloud import datastore
 from flask import Flask, request
-import json
-import constants
-import boat
 
 
 app = Flask(__name__)
-app.register_blueprint(boat.bp)
 
 
 @app.route('/')
 def index():
-    return "Please navigate to /boats to use this API"
+    return "Please god"
 
 
 if __name__ == '__main__':
